374c4c348c623054b8b52b1517b14766551a91c6
migueprograma34 <mr141434@gmail.com>
2023-02-05 02:55:54 -0300

    ++

---
+++ src/main/java/com/backend/migue/security/Entity/Rol.java
@@ -0,0 +1,17 @@
+package com.backend.migue.security.Entity;
+
+import com.backend.migue.security.Enums.RolNombre;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RolNombre rolNombre;
+
+}
