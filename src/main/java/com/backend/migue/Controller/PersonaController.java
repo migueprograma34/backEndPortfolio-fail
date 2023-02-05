fd7178bfba9951f9f25e0fd88d8417a896af6dd0
migueprograma34 <mr141434@gmail.com>
2023-02-05 00:41:53 -0300

    solución de conexión

--- src/main/java/com/backend/migue/Controller/PersonaController.java
+++ src/main/java/com/backend/migue/Controller/PersonaController.java
@@ -4,10 +4,19 @@ import com.backend.migue.Entity.Persona;
 import com.backend.migue.Interface.IPersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     @GetMapping("personas/traer")
@@ -36,5 +45,9 @@ public class PersonaController {
 
         ipersonaService.savePersona(persona);
         return persona;
+    }
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)2);
     }
 }
